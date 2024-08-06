65cba280a58559db1c8e0a058983d3c5ce3a3cd0
Kaidong Hu <dongforregister@gmail.com>
2024-08-06 11:34:42 -0400

    Refactor HTML encoding and update button to link in views
    Refactor HTML game compile helper to include new encode_html method and
    remove markdown fences. Update game project edit button to link and
    adjust iframe height in HTML game compile\
    view.

--- app/helpers/html_game_compiles_helper.rb
+++ app/helpers/html_game_compiles_helper.rb
@@ -1,2 +1,15 @@
 module HtmlGameCompilesHelper
+  def encode_html(html)
+    encode64(fix_html_bytes(html))
+  end
+
+  def fix_html_bytes(html)
+    # remove markdown fences at the beginning and end of the html
+    html.gsub(/^```html\n/, '')
+        .gsub(/```\n?$/, '')
+  end
+
+  def encode64(html)
+    Base64.strict_encode64(html).gsub("\n", '')
+  end
 end
