d37442c2e6de42e13ecd0e4cb8132564fef9d788
Kaidong Hu <dongforregister@gmail.com>
2024-12-17 19:57:05 -0500

    Adjust RuboCop configuration, autocorrect from rubocop recommendations

--- app/services/CustomizeGameService.rb
+++ app/services/CustomizeGameService.rb
@@ -1,14 +1,14 @@
 class CustomizeGameService
   def self.customize_game(params)
-      client = OpenAI::Client.new
-      response = client.chat(parameters: {
-        model: params.fetch(:model, "gpt-4o"),
-        messages: [
-          { role: "system", content: params[:system_prompt] },
-          { role: "system", content: "```html\n#{params[:game_html]}\n```" },
-          { role: "user", content: params[:request] }
-        ]
-      })
-      response.dig("choices", 0, "message", "content")
+    client = OpenAI::Client.new
+    response = client.chat(parameters: {
+                             model: params.fetch(:model, "gpt-4o"),
+                             messages: [
+                               { role: "system", content: params[:system_prompt] },
+                               { role: "user", content: "```html\n#{params[:game_html]}\n```" },
+                               { role: "user", content: params[:request] }
+                             ]
+                           })
+    response.dig("choices", 0, "message", "content")
   end
 end
