b7ecf9b7be8510e043fb821453cab877c175d41c
Kaidong Hu <dongforregister@gmail.com>
2025-04-23 20:33:24 +0000

    Update Ruby version to 3.3.7, add gcloud.key to .gitignore, and enhance devcontainer configuration
    - Updated Ruby version in .ruby-version and Gemfile to 3.3.7.
    - Added /config/gcloud.key to .gitignore to prevent accidental commits of sensitive files.
    - Modified devcontainer.json to use a Ruby 3.3 base image and added Google Cloud keyfile environment variable.
    - Improved postCreateCommand in devcontainer.json to include corepack and yarn installation.

--- config/initializers/mark_compiling_jobs_stale.rb
+++ config/initializers/mark_compiling_jobs_stale.rb
@@ -1,6 +1,9 @@
 Rails.application.config.after_initialize do
+  begin
   unless defined?(Rails::Console) || File.split($0).last == "rake"
     GameCompile.status_compiling.update_all(status: "stale")
     GameCompile.status_pending.update_all(status: "stale")
+  end
+  rescue
   end
 end
